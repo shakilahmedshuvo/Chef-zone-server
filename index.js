2936ecccb1622643dc7fbb41c94b2ce7900083ee
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-10-30 12:05:37 +0600

    get chef data by id api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 //mongodb start 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.frhesy5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,11 +28,19 @@ async function run() {
         await client.connect();
 
         // get the database
-        const chefCollection=client.db('chefZoneDatabase').collection('chefCollection');
+        const chefCollection = client.db('chefZoneDatabase').collection('chefCollection');
 
         // get api for chef collection
-        app.get("/chefCollection", async(req,res)=>{
+        app.get("/chefCollection", async (req, res) => {
             const result = await chefCollection.find().toArray();
+            res.send(result);
+        });
+
+        // college get by id api
+        app.get('/chefCollection/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await chefCollection.findOne(query);
             res.send(result);
         });
 
